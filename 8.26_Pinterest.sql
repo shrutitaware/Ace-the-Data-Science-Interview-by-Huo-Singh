fab110ea0e42fc695ca27660c339e9b26afa022e
Shruti Taware <45720788+shrutitaware@users.noreply.github.com>
2021-09-03 00:11:45 -0700

    Create 8.26_Pinterest.sql

---
+++ 8.26_Pinterest.sql
@@ -0,0 +1,17 @@
+-- Step 1 : self join,  Finding concurrent sessions so no two sessions_ids can be same
+-- Step 2 : Sessons are concurrent if:
+-- 					a. s1 starts first, s2 start >= s1 start
+--					b. s2 starts first, s2 start <= s1 end
+--			i.e s2 start between s1 start and s1 end
+-- COUNT concurrent sessions and find only largest number
+
+
+SELECT TOP (1) 
+			s1.session_id,
+			COUNT(s2.session_id) AS concurrents
+FROM sessions s1 INNER JOIN
+sessions s2 ON s1.session_id!=s2.session_id
+AND s2.start_time BETWEEN s1.start_time
+									AND s1.end_time
+GROUP BY s1.session_id
+ORDER BY concurrents DESC
