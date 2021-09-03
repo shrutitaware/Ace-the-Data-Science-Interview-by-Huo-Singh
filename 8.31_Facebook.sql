e4f68a60a1e0ab2aad5c79e3ab77d33fd2eb9ba5
Shruti Taware <45720788+shrutitaware@users.noreply.github.com>
2021-09-02 21:47:27 -0700

    Update 8.31_Facebook.sql

--- 8.31_Facebook.sql
+++ 8.31_Facebook.sql
@@ -1,10 +1,10 @@
 -- Step 1 : Find all the users who did not login last month: 
---					users between current month and current month - 1 (ie. last month)
+--	    users between current month and current month - 1 (ie. last month)
 -- Step 2 : WHERE EXISTS against previous month
 -- Step 3 : COUNT no. of users satisfying this condition
 
 SELECT
-			DATEPART(MONTH, curr_month.login_date) AS current_month,
+      DATEPART(MONTH, curr_month.login_date) AS current_month,
       COUNT(*) AS num_reactivated_users
 FROM user_logins curr_month
 WHERE EXISTS
